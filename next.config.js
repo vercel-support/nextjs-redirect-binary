bca5366e98796a249fbd0d036f2b4e89c01b534f
Ismael Rumzan <ismael.rumzan@gmail.com>
2021-10-05 14:10:45 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/mail/image.gif',
+        destination: 'https://checkout.mammut/mail/image.gif',
+        permanent: true,
+      },
+    ]
+  },
 }
